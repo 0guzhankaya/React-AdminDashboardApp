1d89b206656a3990806f26f27efaf3866039b058
Oguzhan Kaya <oguzhankayabusiness@gmail.com>
2024-08-24 19:33:57 +0300

    DataGrid tables added and bugs fixed.

--- src/scenes/dashboard/index.jsx
+++ src/scenes/dashboard/index.jsx
@@ -1,10 +1,14 @@
 import React from "react";
+import { Box } from "@mui/material";
+import Header from "../../components/Header";
 
 function Dashboard() {
   return (
-    <div>
-      <div>Dashboard</div>
-    </div>
+    <Box m="20px">
+      <Box display="flex" justifyContent="space-between" alignItems="center">
+        <Header title="Dashboard" subtitle="Welcome to your dashboard" />
+      </Box>
+    </Box>
   );
 }
 
--- src/scenes/global/Sidebar.jsx
+++ src/scenes/global/Sidebar.jsx
@@ -106,10 +106,10 @@ const Sidebar = () => {
                   fontWeight="bold"
                   sx={{ m: "10px 0 0 0" }}
                 >
-                  Ed Roh
+                  Oğuzhan Kaya
                 </Typography>
                 <Typography variant="h5" color={colors.greenAccent[500]}>
-                  VP Fancy Admin
+                  OK Tech Admin
                 </Typography>
               </Box>
             </Box>
